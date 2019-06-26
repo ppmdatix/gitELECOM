e1f75c2083d62687fa73cbe6036f7511885f40e5
ppmdatix <paul.peseux@gmail.com>
2019-06-26 16:10:33 +0200

    swagan results

---
+++ cGANoDEbergerac/resultsOK/swagan_learning_1/config.py
@@ -0,0 +1,13 @@
+epochs = 20
+number_of_gans = 5
+switches = 8
+examples = 100
+smooth_zero = .1
+smooth_one = .9
+latent_dim = 50
+x_train_size = None
+dropout = .2
+leaky_relu = 0.2
+spectral_normalisation=False
+weight_clipping=False
+activation="tanh"
