bedafd4ea9529e014637d53903d41715029a3056
ppmdatix <paul.peseux@gmail.com>
2019-06-27 18:08:48 +0200

    loading data modification

--- cGANoDEbergerac/load_data/load_data.py
+++ cGANoDEbergerac/load_data/load_data.py
@@ -2,18 +2,18 @@ import numpy as np
 import sys
 from sklearn.model_selection import train_test_split
 import pandas as pd
-# sys_path = "/Users/ppx/Desktop/gitELECOM/NSL-KDD"
-sys_path = "/home/peseux/Desktop/gitELECOM/NSL-KDD/"
+sys_path = "/Users/ppx/Desktop/gitELECOM/NSL-KDD"
+# sys_path = "/home/peseux/Desktop/gitELECOM/NSL-KDD/"
 sys.path.insert(0, sys_path)
 from loading.loadingKDD import loadingKDD
 
 
 def load_data(attack_mode=None, nrows=10000000, attack=None,
               verbose=True, shuffle=False,
-              cv_size=0., place="work", log_transform=True):
+              cv_size=0., place="work", log_transform=True, return_colnames=False):
 
     # DATA
-    x_train, y_train, cat_col, _ = loadingKDD(nrows=nrows, attack_mode=attack_mode,
+    x_train, y_train, cat_col, colnames = loadingKDD(nrows=nrows, attack_mode=attack_mode,
                                               attack=attack, force_cat_col=None, place=place, log_transform=log_transform)
     x_test, y_test, _, _ = loadingKDD(nrows=nrows,
                                       attack_mode=attack_mode, attack=attack,
@@ -52,8 +52,13 @@ def load_data(attack_mode=None, nrows=10000000, attack=None,
         print(pd.DataFrame(y_train).head())
         print("\n  \n \n "*2)
     if cv_size == 0.:
-        return x_train, y_train, x_balanced_train, y_balanced_train, x_test, y_test
+        if return_colnames:
+            return x_train, y_train, x_balanced_train, y_balanced_train, x_test, y_test, colnames
+        else:
+            return x_train, y_train, x_balanced_train, y_balanced_train, x_test, y_test
     else:
         x_train, x_train_cv, y_train, y_train_cv = train_test_split(x_train, y_train, test_size=cv_size)
-
-        return x_train, x_train_cv, y_train, y_train_cv, x_balanced_train, y_balanced_train, x_test, y_test
+        if return_colnames:
+            return x_train, x_train_cv, y_train, y_train_cv, x_balanced_train, y_balanced_train, x_test, y_test, colnames
+        else:
+            return x_train, x_train_cv, y_train, y_train_cv, x_balanced_train, y_balanced_train, x_test, y_test
