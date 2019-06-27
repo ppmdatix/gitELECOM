b7ad035b55c0aa30166b68214eb7b1609a9f1751
ppmdatix <paul.peseux@gmail.com>
2019-06-27 18:09:13 +0200

    evaluation perfectioning

--- cGANoDEbergerac/evaluation/evaluation.py
+++ cGANoDEbergerac/evaluation/evaluation.py
@@ -3,8 +3,8 @@ from sklearn.metrics import confusion_matrix as confusion_matrix, precision_scor
 
 def evaluate(y_true, y_pred):
     cm = confusion_matrix(y_true=y_true, y_pred=y_pred)
-    ps = precision_score(y_true=y_true, y_pred=y_pred)
-    rs = recall_score(y_true=y_true, y_pred=y_pred)
-    f1 = f1_score(y_true=y_true, y_pred=y_pred)
-    acc = accuracy_score(y_true=y_true, y_pred=y_pred)
+    ps = .001 * int(1000 * precision_score(y_true=y_true, y_pred=y_pred))
+    rs = .001 * int(1000 * recall_score(y_true=y_true, y_pred=y_pred))
+    f1 = .001 * int(1000 * f1_score(y_true=y_true, y_pred=y_pred))
+    acc = .001 * int(1000 * accuracy_score(y_true=y_true, y_pred=y_pred))
     return {"confusion_matrix": cm, "precision": ps, "recall": rs, "f1_score": f1, "accuracy": acc}
